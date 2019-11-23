f2966e46d80f912bd60f3ebd8ffc0fb8d7cd7467
Arpine <garanyanarpine@gmail.com>
2019-11-23 19:40:23 +0400

    added new files

---
+++ webSite/precache-manifest.8c5f5bfd7f8a4447e99a24777735ecdb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f102c1d0ee773041637e3d33aaeb9b2b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9b519371fb31ebb449ab",
+    "url": "/static/css/main.9f5999a8.chunk.css"
+  },
+  {
+    "revision": "8df61f490f23a59a3575",
+    "url": "/static/js/2.0aca2316.chunk.js"
+  },
+  {
+    "revision": "9b519371fb31ebb449ab",
+    "url": "/static/js/main.fc8e5765.chunk.js"
+  },
+  {
+    "revision": "04496fc9af7fca12cbcb",
+    "url": "/static/js/runtime-main.cd819f53.js"
+  },
+  {
+    "revision": "645ff6ef62897e2a58e8a3425e01e5f9",
+    "url": "/static/media/img.645ff6ef.jpg"
+  }
+]);
