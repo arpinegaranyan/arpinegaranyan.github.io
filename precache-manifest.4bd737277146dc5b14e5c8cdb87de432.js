19d530995de315859a23f9d87dd7f815444aca4b
Arpine <garanyanarpine@gmail.com>
2019-11-27 14:07:53 +0400

    Updates

---
+++ precache-manifest.4bd737277146dc5b14e5c8cdb87de432.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2845f96a6f48897de4ea98de86a6a714",
+    "url": "/index.html"
+  },
+  {
+    "revision": "19582ad5c32caede5e75",
+    "url": "/static/css/main.be731259.chunk.css"
+  },
+  {
+    "revision": "8df61f490f23a59a3575",
+    "url": "/static/js/2.0aca2316.chunk.js"
+  },
+  {
+    "revision": "19582ad5c32caede5e75",
+    "url": "/static/js/main.fc8e5765.chunk.js"
+  },
+  {
+    "revision": "04496fc9af7fca12cbcb",
+    "url": "/static/js/runtime-main.cd819f53.js"
+  },
+  {
+    "revision": "645ff6ef62897e2a58e8a3425e01e5f9",
+    "url": "/static/media/img.645ff6ef.jpg"
+  }
+]);
